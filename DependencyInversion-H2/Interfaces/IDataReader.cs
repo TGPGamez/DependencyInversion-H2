420f5a473e0b02f4d95d90cdbd5c2ec604e33a3c
TGPGamez <58465104+TGPGamez@users.noreply.github.com>
2021-12-09 12:46:24 +0100

    Add project files.

---
+++ DependencyInversion-H2/Interfaces/IDataReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversion_H2.Interfaces
+{
+    interface IDataReader
+    {
+        /// <summary>
+        /// Used to read data
+        /// </summary>
+        /// <returns></returns>
+        string RequestData();
+    }
+}
